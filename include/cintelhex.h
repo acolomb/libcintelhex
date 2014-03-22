186dd8cfeb88283c5a504213aa485579b4199f6e
André Colomb <src@andre.colomb.de>
2014-03-22 12:56:51 +0100

    Add new API function ihex_byte_copy().
    The implementation is basically the same as the old ihex_mem_copy()
    function.  However, the destination memory is not zeroed to avoid
    overwriting content which is not specified in the ihex records.
    
    The copy is done completely byte-wise, avoiding memory access
    violations caused by misalignment.  Record contents whose address lies
    beyond the given destination range are silently discarded instead of
    aborting with an IHEX_ERR_ADDRESS_OUT_OF_RANGE error code.
    
    The new parameter off indicates the start address from which the
    destination range should be filled.  Keep track of the minimum and
    maximum data address within the record set and report an
    IHEX_ERR_ADDRESS_OUT_OF_RANGE error if the source and destination
    ranges did not overlap at all.

--- include/cintelhex.h
+++ include/cintelhex.h
@@ -213,7 +213,9 @@ int ihex_check_record(ihex_record_t *r);
 
 /// Copy the content of a record set.
 /** This method copies the content of a record set to a certain
- *  location in memory.
+ *  location in memory.  The destination memory is completely zeroed
+ *  before copying the record set content.  Record addresses outside
+ *  the given destination range lead to an error.
  * 
  *  @param rs  The record set that is to be copied.
  *  @param dst A pointer to the destination address.
@@ -222,6 +224,21 @@ int ihex_check_record(ihex_record_t *r);
  *  @param o   Defines whether data words are big or little endian.
  *  @return    0 on success, an error code otherwise. */
 int ihex_mem_copy(ihex_recordset_t *rs, void* dst, ulong_t n, ihex_width_t w, ihex_byteorder_t o);
+
+/// Copy the content of a record set byte-wise.
+/** This method copies the content of a record set to a certain
+ *  location in memory.  In contrast to ihex_mem_copy(), the
+ *  destination memory is not zeroed first.  Any record contents whose
+ *  address lies outside the given destination range are silently
+ *  skipped.  An offset causes the copy to start at the specified
+ *  address within the source data.
+ * 
+ *  @param rs  The record set that is to be copied.
+ *  @param dst A pointer to the destination address.
+ *  @param n   The size of the allocated target area.
+ *  @param off Offset address from where to start the copy.
+ *  @return    0 on success, an error code otherwise. */
+int ihex_byte_copy(ihex_recordset_t *rs, char *dst, size_t n, size_t off);
 
 /// Fill a memory area with zeroes.
 /** This method fills a whole memory area with zeros.
